3ae57f6714c11a683dc7fa4fd34667baeb4da432
Douglas Egiemeh <douglasegiemeh@gmail.com>
2019-02-18 20:06:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Simple Next. js/react App that consumes coindesk api to display updates on bitcoin values in differennt currencies.
+
+To use the app:
+- Clone or fork the repository
+- run `npm install`
+- run `npm run dev` to start
+- Navigate to `localhost:3000`
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
+const About = () => (
+  <Layout>
+    <div>
+        <h1>About BitzPrize</h1>
+        <p>Application to checkout bit coin prices</p>
+    </div>
+  </Layout>
+);
+
+export default About;
